d2677fe8a4d85d71ea5322580964c4db432bb714
krptr <254871473+krptr@users.noreply.github.com>
2026-08-12 03:23:30 +0100

    updated some functions

--- supabase/functions/verify-payment/index.ts
+++ supabase/functions/verify-payment/index.ts
@@ -2,7 +2,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { createOrderFromIntent } from "../_shared/order.ts";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://deslu-eatery.netlify.app/",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
 
